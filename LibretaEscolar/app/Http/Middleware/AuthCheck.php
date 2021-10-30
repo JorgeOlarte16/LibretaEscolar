0e041b10c216180b7e322e67af741bab673063ea
JorgeOlarte16 <ja.olarte16@gmail.com>
2021-10-29 23:22:33 -0500

    Update AuthCheck.php

--- LibretaEscolar/app/Http/Middleware/AuthCheck.php
+++ LibretaEscolar/app/Http/Middleware/AuthCheck.php
@@ -17,12 +17,21 @@ class AuthCheck
     public function handle(Request $request, Closure $next)
     {
 
-        if(!session()->has('LoggedUser') &&($request->path() !='auth/login' && $request->path() !='auth/register')){
+        if(!session()->has('LoggedDocente') &&($request->path() !='auth/login' && $request->path() !='auth/register')){
             return redirect('auth/login')->with('fail', 'Debes iniciar sesion');
         }
-        if(session()->has('LoggedUser') && ($request->path() == 'auth/login' || $request->path() == 'auth/register')){
+        if(session()->has('LoggedDocente') && ($request->path() == 'auth/login' || $request->path() == 'auth/register')){
             return back();
         }
+        if(!session()->has('LoggedAcudiente') &&($request->path() !='auth/login' && $request->path() !='auth/register')){
+            return redirect('auth/login')->with('fail', 'Debes iniciar sesion');
+        }
+        if(session()->has('LoggedAcudiente') && ($request->path() == 'auth/login' || $request->path() == 'auth/register')){
+            return back();
+        }
+
+
+
         return $next($request)->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
                               ->header('Pragma','no-cache')
                               ->header('Expires','Sat 01 Jan 1990 00:00:00 GMT');;
